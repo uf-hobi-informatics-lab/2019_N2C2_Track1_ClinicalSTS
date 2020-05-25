f8153c6087cf47bea1bf72a95b5e026f851e2903
Yinghan Ma <ma.yingha@ufl.edu>
2020-05-25 14:26:52 -0400

    Update cross validation eval script name

--- cv_eval.sh
+++ cv_eval.sh
@@ -1,4 +1,4 @@
-python src/cv_result_eval.py \
+python src/cross_valid_eval.py \
   --model_type model_type \  # replace model_type with the model type name you are currently evaluating
   --input_dir output/to/5_fold_cv/tmp \
   --output_dir output/dir/to/eval/result
